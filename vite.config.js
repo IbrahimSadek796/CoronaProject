518d766c2a65dd46113bd00b55e2d8b93e261936
IbrahimSadek796 <118129618+IbrahimSadek796@users.noreply.github.com>
2023-03-14 18:01:53 +0200

    new add

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,13 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// import vueJsx from '@vitejs/plugin-vue-jsx'
+import vuejsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   "base" : "/CoronaProject/",
-  plugins: [vue()],
+  plugins: [vue() , vuejsx],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
